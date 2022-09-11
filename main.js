7110cf0c1968b0b594106f213105051150b611a3
alizubairr <axubair9@gmail .com>
2022-09-11 22:57:49 +0600

    Complete with responsive

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ const circleColorChange = (color) =>{
     const cirlce = document.querySelector('.circle');
     cirlce.style.background = color;
 }
+
+// Nav Bar
+const toggleMenu = () => {
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
